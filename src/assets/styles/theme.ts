88c32f1ba9af48ed0d389637ddc4396f56f7c5a7
Mateusz Cwojdziński <mateuszcwojdzinski@outlook.com>
2023-07-29 21:57:45 +0200

    Add rest of the colors for setting a theme

--- src/assets/styles/theme.ts
+++ src/assets/styles/theme.ts
@@ -1,9 +1,19 @@
 export const darkTheme = {
   bgPrimary: '#050505',
-  textPrimary: 'red'
+  bgSecondary: '#1f1f1f',
+  textPrimary: '#ffffff',
+  textSecondary: '#757575',
+  accentColorPrimary: '#8f19e8',
+  accentColorSecondary: '#a445ed',
+  warning: '#ff5252'
 }
 
 export const lightTheme = {
-   bgPrimary: '#f2f2f2',
-  textPrimary: 'blue'
+  bgPrimary: '#ffffff',
+  bgSecondary: '#f4f4f4',
+  textPrimary: '#2d2d2d',
+  textSecondary: '#757575',
+  accentColorPrimary: '#8f19e8',
+  accentColorSecondary: '#a445ed',
+  warning: '#ff5252'
 }
